8886fb48aa5f792a7ffe136c69b3261c0d448980
Alexandr Tikhonov <t9101029991@gmail.com>
2017-03-25 14:27:05 +0300

    new slide

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -8,8 +8,10 @@ $this->registerJs('
 		$(".fotorama").fotorama({
 			width: "100%",
 			ratio: 2/1,
+			autoplay: 2000,
 			data:[
 				{img: "/images/meet-slider1-background.png", fit: "cover",html: "<img src=\"/images/meet-slider1-image1-reverse.png\" style=\"height:100%\"><img src=\"/images/meet-slider1-image2.png\" style=\"position: absolute;top: -40%;height: 140%;right: 0;\"><div class=\"\" style=\"position: absolute;top: 100px;width: 88%;text-align: center;font-size: 30px;\">ALABAMA / Стол*</div><div style=\"position: absolute;top: 200px;width: 50%;text-align: center;left: 19%;font-size:19px\">Современный, утонченный и изящный стол. Подходитдля классическогои современного интерьера. Идеально в писывается в любое пространство</div><div style=\"bottom: 15px;position: absolute;width: 50%;text-align: center;left: 19%;\">Это изделие ЛИДЕР ПРОДАЖ</div>"},
+				{img: "/images/meet-slide-2.jpg"}
 			],
 		});
 	');
