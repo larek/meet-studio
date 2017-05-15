4af8f88bf1f6a74fab733cf7b84ba34893b8c906
Alexandr Tikhonov <t9101029991@gmail.com>
2017-05-15 12:07:56 +0300

    png in real project

--- basic/views/site/real-project.php
+++ basic/views/site/real-project.php
@@ -8,24 +8,24 @@ $this->registerCssFile('/bower_components/font-awesome/css/font-awesome.min.css'
 <h1><?= $this->title;?></h1>
 <span class='btn-fotorama-fullscreen'><i class='fa fa-arrows-alt'></i> Развернуть на весь экран</span>
 <div class="fotorama" data-width='100%' data-nav-position='top' data-nav='thumbs' data-allowfullscreen='true' data-autoplay='3000'>
-	<img src="/images/real-projects/1/01.jpg" alt="">
-	<img src="/images/real-projects/1/02.jpg" alt="">
-	<img src="/images/real-projects/1/03.jpg" alt="">
-	<img src="/images/real-projects/1/04.jpg" alt="">
-	<img src="/images/real-projects/1/05-1.jpg" alt="">
-	<img src="/images/real-projects/1/05-2.jpg" alt="">
-	<img src="/images/real-projects/1/06.jpg" alt="">
-	<img src="/images/real-projects/1/07.jpg" alt="">
-	<img src="/images/real-projects/1/08.jpg" alt="">
-	<img src="/images/real-projects/1/09.jpg" alt="">
-	<img src="/images/real-projects/1/10.jpg" alt="">
-	<img src="/images/real-projects/1/11.jpg" alt="">
-	<img src="/images/real-projects/1/12.jpg" alt="">
-	<img src="/images/real-projects/1/13.jpg" alt="">
-	<img src="/images/real-projects/1/14.jpg" alt="">
-	<img src="/images/real-projects/1/15.jpg" alt="">
-	<img src="/images/real-projects/1/16.jpg" alt="">
-	<img src="/images/real-projects/1/17.jpg" alt="">
+	<img src="/images/real-projects/1/01.png" alt="">
+	<img src="/images/real-projects/1/02.png" alt="">
+	<img src="/images/real-projects/1/03.png" alt="">
+	<img src="/images/real-projects/1/04.png" alt="">
+	<img src="/images/real-projects/1/05-1.png" alt="">
+	<img src="/images/real-projects/1/05-2.png" alt="">
+	<img src="/images/real-projects/1/06.png" alt="">
+	<img src="/images/real-projects/1/07.png" alt="">
+	<img src="/images/real-projects/1/08.png" alt="">
+	<img src="/images/real-projects/1/09.png" alt="">
+	<img src="/images/real-projects/1/10.png" alt="">
+	<img src="/images/real-projects/1/11.png" alt="">
+	<img src="/images/real-projects/1/12.png" alt="">
+	<img src="/images/real-projects/1/13.png" alt="">
+	<img src="/images/real-projects/1/14.png" alt="">
+	<img src="/images/real-projects/1/15.png" alt="">
+	<img src="/images/real-projects/1/16.png" alt="">
+	<img src="/images/real-projects/1/17.png" alt="">
 </div>
 
 <br>
