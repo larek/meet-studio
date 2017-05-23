1c5944467f92debba304b90087fe1e3964fef4d0
Alexandr Tikhonov <t9101029991@gmail.com>
2017-05-23 13:23:18 +0300

    furniture thumb

--- basic/views/site/furniture.php
+++ basic/views/site/furniture.php
@@ -16,21 +16,21 @@ $this->title = 'Мебель';
 
 <div class="imageblock" onclick="window.location = '/furniture/1'">
 	<div class="singleimage">
-		<img width="300" height="200" oncontextmenu="return false;" src="/images/furniture/1/thumb-tumba-classic-1.jpg" class="attachment-gallery wp-post-image">
+		<img width="300" height="200" oncontextmenu="return false;" src="/images/furniture/1/meet-studio-thumb-furniture-1.png" class="attachment-gallery wp-post-image">
 	</div>
 	<div class="afterpicture">Прикроватная тумба Classic</div>
 </div>
 
 <div class="imageblock" onclick="window.location = '/furniture/2'">
 	<div class="singleimage">
-		<img width="300" height="200" oncontextmenu="return false;" src="/images/furniture/2/thumb-1.jpg" class="attachment-gallery wp-post-image">
+		<img width="300" height="200" oncontextmenu="return false;" src="/images/furniture/2/meet-studio-thumb-furniture-2.png" class="attachment-gallery wp-post-image">
 	</div>
 	<div class="afterpicture">Прикроватная тумба</div>
 </div>
 
 <div class="imageblock" onclick="window.location = '/furniture/3'">
 	<div class="singleimage">
-		<img width="300" height="200" oncontextmenu="return false;" src="/images/furniture/3/thumb-1.jpg" class="attachment-gallery wp-post-image">
+		<img width="300" height="200" oncontextmenu="return false;" src="/images/furniture/3/meet-studio-thumb-furniture-3.png" class="attachment-gallery wp-post-image">
 	</div>
 	<div class="afterpicture">Банкетка</div>
 </div>
