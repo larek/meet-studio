8a6a0f82f125ac16cb02a0175409ba2a430baea4
Alexandr Tikhonov <t9101029991@gmail.com>
2017-06-07 11:26:22 +0300

    change title in decor-1

--- basic/views/site/decor-single-2.php
+++ basic/views/site/decor-single-2.php
@@ -1,9 +1,9 @@
 <?
-$this->title = 'Каменный шпон';
+$this->title = 'Наши финиши';
 $this->registerJsFile('/bower_components/fotorama/fotorama.js',['depends' => [\yii\web\JqueryAsset::className()]]);
 $this->registerCssFile('/bower_components/fotorama/fotorama.css');
 ?>
-<h1>Каменный шпон</h1>
+<h1>Наши финиши</h1>
 <p>Кожа коллекции OLD с эффектом старения! Без тиснения. Сохраняющая на себе прижизненные пороки на шкуре животного. Эта кожа высокого качества и относится к Категории «А».</p>
 <div class="fotorama" data-width='100%' data-nav-position='top' data-nav='thumbs' data-allowfullscreen='true'>
 	<img src="/images/decor/2/1.jpg" alt="">
