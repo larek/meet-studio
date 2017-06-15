177a2cb8c8796ddcb57ca2fe6b480ef36f251cd8
Alexandr Tikhonov <t9101029991@gmail.com>
2017-06-16 01:46:41 +0300

    decor-2 new images

--- basic/views/site/decor-single-2.php
+++ basic/views/site/decor-single-2.php
@@ -2,14 +2,26 @@
 $this->title = 'Наши финиши';
 $this->registerJsFile('/bower_components/fotorama/fotorama.js',['depends' => [\yii\web\JqueryAsset::className()]]);
 $this->registerCssFile('/bower_components/fotorama/fotorama.css');
+$this->registerCssFile("https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css");
+
 ?>
-<h1>Наши финиши</h1>
-<p>Кожа коллекции OLD с эффектом старения! Без тиснения. Сохраняющая на себе прижизненные пороки на шкуре животного. Эта кожа высокого качества и относится к Категории «А».</p>
-<div class="fotorama" data-width='100%' data-nav-position='top' data-nav='thumbs' data-allowfullscreen='true'>
-	<img src="/images/decor/2/1.jpg" alt="">
-	<img src="/images/decor/2/2.jpg" alt="">
-	<img src="/images/decor/2/3.jpg" alt="">
-	<img src="/images/decor/2/4.jpg" alt="">
-	<img src="/images/decor/2/5.jpg" alt="">
-	<img src="/images/decor/2/6.jpg" alt="">
+<div class="row">
+	<div class="col-md-12">
+		<h1>Наши финиши</h1>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-6">
+		<div class="fotorama" data-width='100%' data-nav-position='bottom' data-nav='thumbs' data-allowfullscreen='true'>
+			<img src="/images/decor/2/7.jpg" alt="">
+			<img src="/images/decor/2/8.jpg" alt="">
+			<img src="/images/decor/2/9.jpg" alt="">
+			<img src="/images/decor/2/10.jpg" alt="">
+			<img src="/images/decor/2/11.jpg" alt="">
+			<img src="/images/decor/2/12.jpg" alt="">
+		</div>
+	</div>
+	<div class="col-md-6">
+		<p>Кожа коллекции OLD с эффектом старения! Без тиснения. Сохраняющая на себе прижизненные пороки на шкуре животного. Эта кожа высокого качества и относится к Категории «А».</p>
+	</div>
 </div>
