37efd4ef268184560589a6615c0e6dff4fd75c12
Alexandr Tikhonov <t9101029991@gmail.com>
2017-10-11 14:09:38 +0300

    src from cdnjs

--- basic/assets/AppAsset.php
+++ basic/assets/AppAsset.php
@@ -19,12 +19,12 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'public/css/all.min.css?v=1.1.9',
-        '/bower_components/fotorama/fotorama.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.min.css',
     ];
     public $js = [
         'http://getbootstrap.com/assets/js/vendor/popper.min.js',
-        '/node_modules/bootstrap/dist/js/bootstrap.min.js',
-        '/node_modules/fotorama/fotorama.js'
+        'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.min.js'
 
         // 'public/js/all-min.js?v=1.0.4',
     ];
