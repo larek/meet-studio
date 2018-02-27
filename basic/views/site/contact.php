f9b24d3f53db2f9147656ff61cc374b4673c7fce
Aleksandr Tikhonov <t9101029991@gmail.com>
2018-02-27 10:27:30 +0300

    update contact info

--- basic/views/site/contact.php
+++ basic/views/site/contact.php
@@ -7,46 +7,27 @@ use yii\helpers\Html;
 $this->title = "Контакты";
 // $this->params['breadcrumbs'][] = ['label' => $mod];
 ?>
-<div id="row">
-                
-    <div class="contact-left">
-        <div id="info-contacts">
-            <div class="info-contacts2 firstt">
-                <p><b>г. КЕМЕРОВО</b> <br>
-                ул. Мичурина 13<br>
-                оф. 104<br>
-                салон Vendixs <br>
-                <!-- Email: <a href="mailto:ppinho@brabbu.com">ppinho@brabbu.com</a><br> -->
-                <!-- Skype:  patriciapinhocunha<br> -->
-                Телефон: <span class="phone">+7 (3842) 58-22-57</span></p>
-            </div>
-            <div class="info-contacts2 ">
-                <p><b>г. НИЖНИЙ НОВГОРОД</b> <br>
-                ул. Ванеева 6<br>
-                <!-- оф. 104<br> -->
-                салон Vendixs<br>
-                <!-- Skype:  patriciapinhocunha<br> -->
-                Офис: <span class="phone">+7 (831) 284-40-24</span><br>
-                Cотовый: <span class="phone">+7 (905) 192-12-34</span></p>
-            </div>
-        
-            <div class="clear"></div>
-        </div>
+<div class="row mt-4">
+    <div class="col-md-4">
+      <p><b>г. МОСКВА</b> <br>
+      Истринский район<br/>
+      КП "Кембридж"<br/>
+      дом 15.4<br/>
+      Cотовый: +7 (905) 192-12-34</p>
     </div>
-<br>
-<div class='map'>
+    <div class="col-md-4">
+      <p><b>г. НИЖНИЙ НОВГОРОД</b> <br>
+      ул. Ванеева 6<br>
+      салон Vendixs<br>
+      Офис: +7 (831) 284-40-24<br>
+      Cотовый: +7 (905) 192-12-34</p>
+    </div>
+    <div class="col-md-4">
+        <p><b>г. КЕМЕРОВО</b> <br>
+        ул. Мичурина 13<br>
+        оф. 104<br>
+        салон Vendixs <br>
+        Телефон: +7 (3842) 58-22-57</p>
+    </div>   
+</div>
    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=C5o6XYsK0Kjw5OORD_-wI4MVdRV-JbQb&amp;width=100%25&amp;height=350&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=true"></script> 
-</div>
-    
-</div>
-<hr>
-            
-<div class="clear"></div>
-
-        
-
-    
-
-
-
-
