3cb34cfbb773b25ea9163976f07733fbaf1fd20a
Alexandr Tikhonov <t9101029991@gmail.com>
2018-03-24 23:13:31 +0300

    real-project page optimize

--- basic/views/site/real-project.php
+++ basic/views/site/real-project.php
@@ -1,29 +1,38 @@
 <?
 $this->title = 'Реализованные проекты';
 $this->registerCssFile('/bower_components/font-awesome/css/font-awesome.min.css');
+$this->registerCssFile('https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css');
+$this->registerJsFile(
+    'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js',
+    ['depends' => [\yii\web\JqueryAsset::className()]]
+);
 ?>
-<h1><?= $this->title;?></h1>
-<span class='btn-fotorama-fullscreen'><i class='fa fa-arrows-alt'></i> Развернуть на весь экран</span>
-<div class="fotorama" data-width='100%' data-nav-position='top' data-nav='thumbs' data-allowfullscreen='true' data-autoplay='3000'>
-	<img src="/images/real-projects/1/01.png" alt="">
-	<img src="/images/real-projects/1/02.png" alt="">
-	<img src="/images/real-projects/1/03.png" alt="">
-	<img src="/images/real-projects/1/04.png" alt="">
-	<img src="/images/real-projects/1/05-1.png" alt="">
-	<img src="/images/real-projects/1/05-2.png" alt="">
-	<img src="/images/real-projects/1/06.png" alt="">
-	<img src="/images/real-projects/1/07.png" alt="">
-	<img src="/images/real-projects/1/08.png" alt="">
-	<img src="/images/real-projects/1/09.png" alt="">
-	<img src="/images/real-projects/1/10.png" alt="">
-	<img src="/images/real-projects/1/11.png" alt="">
-	<img src="/images/real-projects/1/12.png" alt="">
-	<img src="/images/real-projects/1/13.png" alt="">
-	<img src="/images/real-projects/1/14.png" alt="">
-	<img src="/images/real-projects/1/15.png" alt="">
-	<img src="/images/real-projects/1/16.png" alt="">
-	<img src="/images/real-projects/1/17.png" alt="">
+<div class="row mt-3">
+  <div class="col-md-12">
+    <h1><?= $this->title;?></h1>
+  </div>
 </div>
-
-<br>
-<iframe width="100%" height="315" src="https://www.youtube.com/embed/LqOyETG6EAM" frameborder="0" allowfullscreen></iframe>
+<div class="row">
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/1.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/1.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/2.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/2.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/3.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/3.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/4.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/4.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/5-1.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/5-1.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/5-2.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/5-2.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/5.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/5.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/6.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/6.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/7.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/7.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/8.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/8.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/9.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/9.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/10.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/10.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/11.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/11.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/12.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/12.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/13.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/13.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/14.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/14.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/15.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/15.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/16.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/16.jpg" class='img-fluid' alt=""></a></div>
+  <div class="col-md-2 mt-3"><a href='/images/real-projects/1/jpg/17.jpg' data-fancybox='group'><img src="/images/real-projects/1/jpg/thumbs/17.jpg" class='img-fluid' alt=""></a></div>
+</div>
+<div class="row mt-3">
+  <iframe width="100%" height="315" src="https://www.youtube.com/embed/LqOyETG6EAM" frameborder="0" allowfullscreen></iframe>
+</div>
